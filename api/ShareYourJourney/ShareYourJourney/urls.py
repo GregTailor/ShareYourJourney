137b106a7ac6144a1bf1effca19c4d26924ebf6d
GregTailor <gregory.tailor98@gmail.com>
2020-06-16 19:29:25 +0200

    added Profile, Country, City models

--- api/ShareYourJourney/ShareYourJourney/urls.py
+++ api/ShareYourJourney/ShareYourJourney/urls.py
@@ -4,7 +4,10 @@ from quickstart import views
 
 router = routers.DefaultRouter()
 router.register(r'users', views.UserViewSet)
+router.register(r'countries', views.CountryView)
+router.register(r'profiles', views.ProfileView)
 router.register(r'groups', views.GroupViewSet)
+router.register(r'cities', views.CityViewSet)
 
 # Wire up our API using automatic URL routing.
 # Additionally, we include login URLs for the browsable API.
--- api/ShareYourJourney/quickstart/serializers.py
+++ api/ShareYourJourney/quickstart/serializers.py
@@ -1,14 +1,36 @@
-from django.contrib.auth.models import User, Group
+from django.contrib.auth.models import Group, User
 from rest_framework import serializers
 
+from quickstart.models import Profile, Country, City
 
-class UserSerializer(serializers.HyperlinkedModelSerializer):
+
+class UserSerializer(serializers.ModelSerializer):
     class Meta:
         model = User
-        fields = ['url', 'username', 'email', 'groups']
+        fields = ['username', 'email', 'groups', 'profile']
+        depth = 1
 
 
-class GroupSerializer(serializers.HyperlinkedModelSerializer):
+class CountrySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Country
+        fields = ['id', 'name', 'city_set']
+        depth = 1
+
+
+class ProfileSerializer(serializers.ModelSerializer):
+    class Meta:
+        model = Profile
+        fields = ['address', 'user']
+
+
+class GroupSerializer(serializers.ModelSerializer):
     class Meta:
         model = Group
-        fields = ['url', 'name']
+        fields = ['name']
+
+
+class CitySerializer(serializers.ModelSerializer):
+    class Meta:
+        model = City
+        fields = ['name', 'country']
